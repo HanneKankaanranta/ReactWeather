27f992392678249ec16752372cf96c78bf2e80e0
Hanne Kankaanranta <hannekan@gmail.com>
2017-03-11 15:37:13 +0200

    Add jquery and foundation

--- app/app.jsx
+++ app/app.jsx
@@ -6,6 +6,10 @@ var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
 
+// Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
 // toinen tapa var Route = require('react-router').Route; jokaiselle erikseen
 // eli tässä tehdään näin:
 //
